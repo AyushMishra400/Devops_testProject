6926f4c3c30a3080d4f5cbce28133cb1eda033a6
Ayush Mishra <mishraayush400@gmail.com>
2020-05-10 13:21:34 +0530

    Update Calculator.java

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -5,8 +5,8 @@ public class Calculator {
 	public static void main(String[] args){
 		System.out.println("100+ 50 = " + add(100, 50));
 		System.out.println("100 - 50 = " + subtract(100,50));
-// 		System.out.println("100 * 50 = " + multiply(100,50));
-// 		System.out.println("100 / 50 = " + divide(100,50));
+		System.out.println("100 * 50 = " + multiply(100,50));
+		System.out.println("100 / 50 = " + divide(100,50));
 	}
 
 	public static int add(int a, int b) {
@@ -17,16 +17,16 @@ public class Calculator {
 		return a - b;
 	}
 
-// 	public static long multiply(int a, int b) {
-// 		return a * b;
-// 	}
-// 	public static double divide(int a, int b) {
-// 		double result;
-// 		if (b == 0) {
-// 			throw new IllegalArgumentException("Divisor cannot divide by zero");
-// 		} else {
-// 			result = Double.valueOf(a)/Double.valueOf(b);
-// 		}
-// 		return result;
-// 	}
+	public static long multiply(int a, int b) {
+		return a * b;
+	}
+	public static double divide(int a, int b) {
+		double result;
+		if (b == 0) {
+			throw new IllegalArgumentException("Divisor cannot divide by zero");
+		} else {
+			result = Double.valueOf(a)/Double.valueOf(b);
+		}
+		return result;
+	}
 }
